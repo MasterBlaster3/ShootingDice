f2c8c23ae25eb781a4d489cad70b952650409f52
Joe Maifeld <josephmaifeld@gmail.com>
2022-09-06 14:15:05 -0500

    fin human player phase

--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -1,10 +1,16 @@
+using System;
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A player the prompts the user to enter a number for a roll
-    public class HumanPlayer
+    public class HumanPlayer : Player
     {
-
+        public override int Roll()
+        {
+            Console.WriteLine("Enter a roll 1-6:");
+            int chosenHumanRoll = Int32.Parse(Console.ReadLine());
+            return chosenHumanRoll;
+        }
     }
 }
--- OneHigherPlayer.cs
+++ OneHigherPlayer.cs
@@ -5,7 +5,7 @@ namespace ShootingDice
     // TODO: Complete this class
 
     // Override the Play method to make a Player who always roles one higher than the other player
-    public class OneHigherPlayer
+    public class OneHigherPlayer : Player
     {
         public override void Play(Player other)
         {
